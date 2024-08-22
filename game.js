244d55e797f4a31453fb5664702ed9c795ce4602
bss1999 <59215767+bss1999@users.noreply.github.com>
2024-08-22 15:41:24 +0900

    게임 업데이트
    모든 구현사항 완료

--- game.js
+++ game.js
@@ -2,72 +2,100 @@ import chalk from 'chalk';
 import readlineSync from 'readline-sync';
 
 
+function random_Number() {return Math.random()}
+
+
 class Player {
   constructor() {
     this.hp = 100; //체력
-    this.power = 10; //공격력
-    this.voidrate = 100; //도망확률
+    this.power = 15; //공격력
+    this.voidrate = 50; //도망확률
     this.defense = 5; //방어수치
-    this.critical = 0; //치명타확률
+    this.counter = 20; //반격확률
+    this.critical = 10; //치명타확률
   }
 
   attack() {
     // 플레이어의 공격
-    // monster.hp -= player.power
-    return function () {
-      monster.hp -= player.power
+    return Math.floor(Math.random() * 11) + this.power
     }
-  }
   
   run() {
     // 플레이어의 도망
-    
+    return Math.floor(Math.random() * 101)
   }
 }
 
 class Monster {
   constructor(stage) {
-    this.hp = Math.floor(80+(stage**Math.random()*10*stage));
-    this.power = Math.floor(3+(stage**Math.random()*10));
+    this.hp = 60;
+    this.power = 10;
   }
 
   attack() {
     // 몬스터의 공격
+    return Math.floor(Math.random() * 6) + this.power
   }
 }
 
-function displayStatus(stage, player, monster) {
+function displayStatus(stage, player, monster) { //스테이터스 표기
   console.log(chalk.magentaBright(`\n=== Current Status ===`));
+  console.log(chalk.cyanBright(`| Stage: ${stage} |`))
   console.log(
-    chalk.cyanBright(`| Stage: ${stage} |`) +
     chalk.blueBright(
         //플레이어 정보 출력
-      `| player HP: ${player.hp}, Attack: ${player.power}`,
-    ) +
-    chalk.redBright(
-        //몬스터 정보 출력
-      `| Monster HP: ${monster.hp}, Attack: ${monster.power} |`,
-    ),
+      `| player 체력: ${player.hp}, 공격력: ${player.power}, 치명타 확률 : ${player.critical}%, 방어력 : ${player.defense}, 반격 : ${player.counter}%, 도망 확률 : ${player.voidrate}%`,
+    ) 
   );
-  console.log(player)
+  console.log(chalk.redBright(
+    //몬스터 정보 출력
+  `| Monster 체력: ${monster.hp}/${mons_maxhp}, 공격력: ${monster.power} |`,
+),)
   console.log(chalk.magentaBright(`=====================\n`));
 }
 
-let reward = ["체력 증가", "공격력 증가", "방어력 증가", "도망 확률 증가", "크리티컬확률 증가"] //능력치 보상 종류
+const reward = ["체력", "공격력", "방어력", "도망", "크리티컬"] //능력치 보상 종류
+let logs = [];
+let mons_maxhp=0, mons_maxpower=0 //몬스터의 최대체력 저장 - 스테이지가 넘어감에따라 강하게 만들기위해
+
 
 const battle = async (stage, player, monster) => {
-  let logs = [];
-  let i=1;
+  // let logs = [];
+  let i=1; // 턴표시를 위한 반복자 / 새 전투가 시작되면 초기화
+  let dmg = 0; // 데미지 표기를 위한 변수
+  let _defense = 0 //턴이 넘어감에따라 방어력 초기화
+  let hp_increase = 0, pw_increase = 0; //몬스터의 스테이지당 증감량
 
-  if(stage===10) {
-  console.log("마지막 적이 나타났습니다! [주의:마지막 적은 도망칠수없습니다.]")
+  if (stage === 1) {//몬스터의 스테이지마다의 스탯 조정
+    mons_maxhp = 60
+    mons_maxpower = 10
+  } 
+  else if (stage > 1 && stage !== 10) {
+  hp_increase = Math.floor(random_Number() * 11) + 10
+  monster.hp = mons_maxhp + hp_increase
+  pw_increase = Math.floor(random_Number() * 4) + 1
+  monster.power = mons_maxpower + pw_increase
+  }
+  else if (stage===10) { //보스 몬스터
+    hp_increase = Math.floor(random_Number() * 11) + 20
+    monster.hp = mons_maxhp + hp_increase
+    pw_increase = Math.floor(random_Number() * 6) + 5
+    monster.power = mons_maxpower + pw_increase
+  }
+
+  mons_maxhp += hp_increase //최대체력 저장
+  mons_maxpower += pw_increase// 
+
+
+  if(stage !== 10) { //몬스터 종류
+    logs.push("몬스터가 나타났습니다!")
   }
   else {
-    console.log("몬스터가 나타났습니다!")
+    logs.push("보스가 나타났습니다! [주의:보스한테서는 도망칠수없습니다.]")
   }
 
   while(player.hp > 0) {
-    // console.clear();
+    _defense = 0;
     displayStatus(stage, player, monster);
 
     logs.forEach((log) => console.log(log));
@@ -79,12 +107,6 @@ const battle = async (stage, player, monster) => {
     );
     const choice = readlineSync.question('당신의 선택은? ');
 
-    console.clear();
-
-    
-    // 플레이어의 선택에 따라 다음 행동 처리
-    // logs.push(chalk.green(`[턴${i}]${choice}를 선택하셨습니다.`));
-    
     if(logs.length > 5) {
       while(1) 
         {
@@ -93,70 +115,104 @@ const battle = async (stage, player, monster) => {
         }
     } //선택 로그가 5개이상 출력되면 로그를 제거
 
-    switch(choice) {
+
+    switch(choice) { // 플레이어의 선택에 따라 다음 행동 처리
       case "1" : //공격 선택지
-        // monster.hp -= player.power;
-        player.attack()
-        monster.hp -= Math.floor(Math.random() * player.power) + 10
-        logs.push(chalk.red(`[${i}] [플레이어의 공격!] 몬스터에게 ${player.power}만큼의 피해를 입혔습니다.`));
+        const cri = Math.floor(Math.random() * 101)
+        dmg = player.attack()
+        if(cri > player.critical) { // 치명타 계산
+          monster.hp -= dmg
+          logs.push(chalk.green(`[${i}] [플레이어의 공격!] 몬스터에게 ${dmg}만큼의 피해를 입혔습니다.`));
+        }
+        else if (cri <= player.critical)
+        {
+          monster.hp -= dmg * 2
+          logs.push(chalk.yellow(`[${i}] [플레이어의 치명타 공격!] 몬스터의 급소를 공격해 ${dmg*2}만큼의 피해를 입혔습니다.`));
+        }
         break;
       case "2" : //방어 선택지
+        _defense += player.defense
+        logs.push(chalk.green(`[${i}] [방어] 플레이어가 방어자세를 취해 ${_defense}만큼의 데미지를 경감합니다!`))
         break;
       case "3" : //도망 선택지
-        let rate = Math.random() * 100
+        const run_rate = player.run()
         if(stage===10) {
           logs.push(`[${i}] [불가능] 마지막 적으로부터는 도망칠 수 없습니다!`)
           break;
         }
-        else if (rate<player.voidrate) {
-          monster.hp = 0;
-          logs.push(`[${i}] [도망 성공!] 적으로부터 도망쳤습니다.`)
+        else if (run_rate<=player.voidrate) {
+          monster.hp = null;
           break;
         }
-        else if(rate>player.voidrate) {
+        else if(run_rate>player.voidrate) {
           logs.push(`[${i}] [도망 실패] 적으로부터 도망치려했지만 실패했습니다.`)
           break;
         }
     }
+
+    if(monster.hp==null) {
+      logs.push(`[${i}] [도망 성공!] 적으로부터 도망쳤습니다.`)
+      logs.push(`도망치는것에 성공하여 체력 30이 회복됩니다.`)
+      player.hp += 30;
+      break;
+    }
     
+    
+    let monsteratk = monster.attack(); //몬스터의 턴
+    dmg = monsteratk - _defense;
+    
+    if (dmg > 0) {
+    player.hp -= dmg
+    logs.push(chalk.red(`[${i}] [몬스터의 공격!]몬스터가 플레이어에게 ${dmg}만큼의 피해를 입혔습니다!`))
+    }
+    else if (dmg <= 0) {
+      logs.push(chalk.red(`[${i}] [몬스터의 공격!] 몬스터가 공격했지만 피해를 입히지 못했습니다.`))
+    }
 
-    monster.attack();
-    player.hp -= Math.floor(Math.random() * monster.power) + 5
-    logs.push(chalk.red(`[${i}] [몬스터의 공격!]몬스터가 플레이어에게 ${monster.power}만큼의 피해를 입혔습니다!`))
+    if(_defense !== 0) {
+      const counter = Math.floor(random_Number() * 101)
+      if(counter < player.counter) {
+         dmg = monsteratk
+         monster.hp -= dmg
+        logs.push(chalk.blue(`[${i}] [반격!] 반격했습니다! 몬스터의 데미지인 ${dmg}만큼 피해를 가합니다.`))
+      }
+    }
 
-    if(monster.hp <= 0) {
-      console.log("몬스터가 쓰러졌습니다.");
-      console.log("스테이지 클리어!");
-      console.log("보상으로 체력50회복과 능력치가 주어집니다.")
-      player.hp += 50;
-      let rewardselect = Math.floor(Math.random() * 4)
-      switch (rewardselect) { //능력치 보상 (구현안됨)
-        case "체력 증가" :
-          hp_up = Math.floor(Math.random() * 30) + 20
-          console.log(`능력치 : ${reward[rewardselect]} - ${hp_up}증가`)
+
+    if(monster.hp <= 0) { // 스테이지 클리어 조건
+      logs.push("[스테이지 클리어!]몬스터가 쓰러졌습니다. 보상으로 체력80회복과 능력치가 주어집니다.");
+      player.hp += 80;
+      const rewardselect = Math.floor(Math.random() * 5)
+      switch (rewardselect) { //능력치 보상
+        case 0 :
+          const hp_up = Math.floor(random_Number() * 31) + 20
+          logs.push(`능력치 : ${reward[rewardselect]} > ${hp_up} 증가`)
           player.hp += hp_up
           break;
-        case "공격력 증가" :
-          pw_up = Math.floor(Math.random() * 15) + 5
-          console.log(`능력치 : ${reward[rewardselect]} - ${pw_up}증가`)
+        case 1 :
+          const pw_up = Math.floor(random_Number() * 16) + 5
+          logs.push(`능력치 : ${reward[rewardselect]} > ${pw_up} 증가`)
           player.power += pw_up
           break;
-        case "방어력 증가" :
-          dp_up = Math.floor(Math.random() * 20) + 10
-          console.log(`능력치 : ${reward[rewardselect]} - ${dp_up}증가`)
+        case 2 :
+          const dp_up = Math.floor(random_Number() * 7) + 4
+          const count_up = Math.floor(random_Number() * 13) + 3
+          logs.push(`능력치 : ${reward[rewardselect]} > ${dp_up} 증가, 반격확률 > ${count_up} 증가`)
           player.defense += dp_up
+          player.counter += count_up
           break;
-        case "도망 확률 증가" :
-          run_up = Math.floor(Math.random() * 10) + 5
-          console.log(`능력치 : ${reward[rewardselect]} - ${run_up}증가`)
+        case 3 :
+          const run_up = Math.floor(random_Number() * 11) + 5
+          logs.push(`능력치 : ${reward[rewardselect]} > ${run_up} 증가`)
           player.voidrate += run_up
           break;
-        case "크리티컬확률 증가" :
-          cri_up = Math.floor(Math.random() * 10) + 5
-          console.log(`능력치 : ${reward[rewardselect]} - ${cri_up}증가`)
+        case 4 :
+          const cri_up = Math.floor(random_Number() * 11) + 5
+          logs.push(`능력치 : ${reward[rewardselect]} > ${cri_up} 증가`)
           player.critical += cri_up
           break;
       }
+
       break;}
 
       i++; //몇번째 턴의 선택인지 표기
@@ -165,7 +221,7 @@ const battle = async (stage, player, monster) => {
 };
 
 export async function startGame() {
-  console.clear();
+  // console.clear();
   const player = new Player();
   let stage = 1;
 
